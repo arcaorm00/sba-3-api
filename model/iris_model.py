c03aa2988782d9ac0665303f1da10365b191142f
salt <arcaorm00@gmail.com>
2020-10-05 16:22:52 +0900

    16:19

--- model/iris_model.py
+++ model/iris_model.py
@@ -8,5 +8,10 @@ class IrisModel:
         print(df.head())
         print('-------------------------')
         print(df.tail())
+        print('-------------------------')
+        print(df.columns)
+        '''
+        Int64Index([0, 1, 2, 3, 4], dtype='int64')
+        '''
 
     
