2c76db96799b34e10043f2dbd20c3e1df4d9f2b2
salt <arcaorm00@gmail.com>
2020-10-06 12:50:42 +0900

    12:50

--- model/seoul_cctv.py
+++ model/seoul_cctv.py
@@ -12,12 +12,20 @@ class Seoulcctv:
         # print(f'basedir: {baseurl}')
         self.fileReader = FileReader()
 
+    def hook_process(self): # hook 메소드: 처리 순서
+        print('--------------- CCTV & POP ---------------')
+        cctv = self.get_cctv()
+        pop = self.get_pop()
+        print(f'CCTV Header: \n{cctv.head()}')
+        print(f'POP Header: \n{pop.head()}')
+
     def get_cctv(self):
         reader = self.fileReader
         reader.context = os.path.join(baseurl, 'data')
         reader.fname = 'cctv_in_seoul.csv'
         cctv = reader.csv_to_dframe()
-        print(cctv)
+        cctv.rename(columns={cctv.columns[0]: '구별'}, inplace = True)
+        # print(cctv)
         return cctv
 
     def get_pop(self):
@@ -25,14 +33,41 @@ class Seoulcctv:
         reader.context = os.path.join(baseurl, 'data')
         reader.fname = 'pop_in_seoul.xls'
         pop = reader.xls_to_dframe(2, 'B, D, G, J, N')
-        print(pop)
+        pop.rename(columns={
+            pop.columns[0]: '구별',
+            pop.columns[1]: '인구수',
+            pop.columns[2]: '한국인',
+            pop.columns[3]: '외국인',
+            pop.columns[4]: '고령자'
+            }, inplace = True)
+        print(f"POP Null Check: {pop['구별'].isnull()}")
+        pop.drop([26], inplace=True) # 원본에서 삭제한다.
+        # print(pop)
         return pop
 
+    """
+        고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
+                                    [-0.28078554  1.        ]] 
+        외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
+                                    [-0.13607433  1.        ]]
+       r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
+       r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
+       r이 -0.3과 -0.1 사이이면, 약한 음적 선형관계,
+       r이 -0.1과 +0.1 사이이면, 거의 무시될 수 있는 선형관계,
+       r이 +0.1과 +0.3 사이이면, 약한 양적 선형관계,
+       r이 +0.3과 +0.7 사이이면, 뚜렷한 양적 선형관계,
+       r이 +0.7과 +1.0 사이이면, 강한 양적 선형관계
+       고령자비율 과 CCTV 상관계수 [[ 1.         -0.28078554] 약한 음적 선형관계
+                                   [-0.28078554  1.        ]]
+       외국인비율 과 CCTV 상관계수 [[ 1.         -0.13607433] 거의 무시될 수 있는
+                                   [-0.13607433  1.        ]]                        
+    """
+    def show_corrcoef(self):
+        pass
 
 if __name__ == '__main__':
     model = Seoulcctv()
-    model.get_cctv()
-    model.get_pop()
+    model.hook_process()
 
 
     
