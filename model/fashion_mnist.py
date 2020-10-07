a712c2c9a60ae1eb84222a961c643318bc7b6cae
salt <arcaorm00@gmail.com>
2020-10-07 17:17:13 +0900

    17:17

--- model/fashion_mnist.py
+++ model/fashion_mnist.py
@@ -75,7 +75,34 @@ class FashionMnist:
         answer = test_labels[index]
         print(f'모델의 예측 값: {np.argmax(pred)}')
         print(f'정답: {answer}')
+        return [predictions, test_images, test_labels]
 
+    def plot_image(self, i, predictions_array, true_label, img):
+        predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(img, cmap=plt.cm.binary)
+
+        prediction_label = np.argmax(predictions_array) # 코트, 신발, 티셔츠 등등 확률로 나온 중 가장 큰 값
+        if prediction_label == true_label:
+            color = 'blue'
+        else:
+            color = 'red'
+        plt.xlabel('{}: {}%' .format(self.class_names[prediction_label], 
+        100 * np.max(predictions_array), self.class_names[true_label], color))
+
+    @staticmethod
+    def plot_value_array(i, predictions_array, true_label):
+        predictions_array, true_label = predictions_array[i], true_label[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        thisplot = plt.bar(range(10), predictions_array, color='#777777')
+        plt.ylim([0, 1])
+        prediction_label = np.argmax(predictions_array)
+        thisplot[prediction_label].set_color('red')
+        thisplot[true_label].set_color('blue')
 
 if __name__ == '__main__':
     fashion = FashionMnist()
