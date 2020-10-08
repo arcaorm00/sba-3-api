139857cbaa380d5e95554aa11f6abfd71916a034
salt <arcaorm00@gmail.com>
2020-10-08 16:48:27 +0900

    16:48

--- service/save_load.py
+++ service/save_load.py
@@ -5,6 +5,7 @@ baseurl = os.path.dirname(os.path.abspath(__file__))
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from util.file_helper import FileReader
 import util.version_checker as version_checker
 
 class SaveLoad:
@@ -15,13 +16,14 @@ class SaveLoad:
     test_labels: object = None
 
     def __init__(self):
+        self.reader = FileReader()
         version_checker.env_info()
 
     def hook(self):
         self.get_data()
         self.create_model()
         self.train_model()
-        self.eval_model()
+        self.save_model()
 
     def get_data(self):
         (self.train_images, self.train_labels), (self.test_images, self.test_labels)\
@@ -46,23 +48,30 @@ class SaveLoad:
         validation_data = (self.test_images, self.test_labels), callbacks=[cp_callback]) # 훈련 단계 콜백 전달
         self.model.load_weights(checkpoint_path) # 가중치 추가
         loss, acc = self.model.evaluate(self.test_images, self.test_labels, verbose=2)
+        # verbose는 학습 진행상황을 보여줄지 말지에 대한 옵션
         print('복원된 모델의 정확도: {:5.2f}%' .format(100 * acc))
         # 파일 이름에 에포크 번호를 포함시킨다.
-        checkpoint_path = 'training_2/cp-{epoch: o4d}.ckpt'
+        checkpoint_path = os.path.join(baseurl, 'training_2', 'cp-{epoch: 04d}.ckpt')
         checkpoint_dir = os.path.dirname(checkpoint_path)
         cp_callback = tf.keras.callbacks.ModelCheckpoint(
             checkpoint_path, verbose = 1, save_weights_only=True,
             period = 5 # 5번째 에포크마다 가중치를 저장한다.  
         )
-        self.model.save_weights(checkpoint_path.format(epochs=0))
-        self.model.fit(self.train_images, self.train_labels, epochs=50, callbacks=[cp_callback], validation_data=(self.test_images, self.test_labels), verbose=0)
+        print(f'checkpoint: {checkpoint_path}')
+        self.model.save_weights(checkpoint_path.format(epoch=0))
+        self.model.fit(self.train_images, self.train_labels, epochs=50, 
+        callbacks=[cp_callback], validation_data=(self.test_images, self.test_labels), verbose=0)
 
     # 전체 모델을 HDF5 파일로 저장한다.
     def save_model(self):
-        self.model.save()
+        context = os.path.join(baseurl, 'saved_model')
+        self.model.save(os.path.join(context, 'my_model.h5'))
+        print('=' * 30)
     
     def load_model(self):
-        pass
+        self.new_model = keras.models.load_model('my_model.h5')
+        self.new_model.summary()
+        loss, acc = self.new_model.evaluate(self.test_images, self.test_labels, verbose = 2)
 
     def debug_model(self):
         print(f'모델정보: {self.model.summary()}')
